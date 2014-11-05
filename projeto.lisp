e9c7bfebd9f7d21b2658b13b2d6a67b963c5e3eb
ruimams <ruimams@gmail.com>
2014-11-05 23:22:37 +0000

    Precisa do first pq o domínio é uma lista.

--- projeto.lisp
+++ projeto.lisp
@@ -25,8 +25,8 @@
 
 (defun psr-atribuicoes (p)
   "Retorna uma lista com todas as atribuicoes - pares (variavel . valor) - do PSR."
-  (mapcan #'(lambda (x y) (when (= (length y) 1) (cons x y))) 
-	      (psr-variaveis-todas p) (psr-dominios p)))
+  (remove nil (mapcar #'(lambda (v d) (when (= (length d) 1) (cons v (first d))))
+	      (psr-variaveis-todas p) (psr-dominios p))))
 	 
 ;;; psr-variaveis-todas: PSR -> lista variaveis
 
