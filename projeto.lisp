b990eab4f86db44d1c4861c0f6e7959d79a66245
Jorgixhallo <jorgixhallo_cs@hotmail.com>
2014-11-05 22:28:00 +0000

    Update projeto.lisp

--- projeto.lisp
+++ projeto.lisp
@@ -72,11 +72,11 @@
   ""
   (setf (nth (position v (psr-variaveis-todas p) :test #'equal) (psr-dominios p)) NIL))
 
-;;; psr-altera-dominio!: PSR x variavel x dominio {}
+;;;;; psr-altera-dominio!: PSR x variavel x dominio {}
 (defun psr-altera-dominio! (p v d)
   ""
   (setf (nth (position v (psr-variaveis-todas p) :test #'equal) (psr-dominios p)) d))
-
+  
 ;;; psr-completo-p: PSR -> logico
 (defun psr-completo-p (p)
   (if (null (psr-variaveis-nao-atribuidas p)) T NIL))
