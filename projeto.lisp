b5b6fb1d27b9099e274def9ab8fab5e3d667a874
ruimams <ruimams@gmail.com>
2014-11-08 18:25:20 +0000

    Almost there...

--- projeto.lisp
+++ projeto.lisp
@@ -177,7 +177,7 @@
 								 (= (first d) 0))))) 
 						 vav)))))
 	     restricoes)))))
-    (cria-psr (nreverse variaveis) (nreverse dominios) restricoes)))
+    (cria-psr (nreverse variaveis) (nreverse dominios) (nreverse restricoes))))
 
 
 (defun lista->lista2d (lista linhas colunas)
@@ -199,12 +199,28 @@
 ;;; procura-retrocesso-simples: PSR -> PSR, inteiro
 (defun procura-retrocesso-simples (p)
   ""
-  (if (psr-completo-p p)
-      p
-      (let ((v (first (psr-variaveis-nao-atribuidas p))))
-	)))
+  (let ((testes-total 0))
+    (if (psr-completo-p p)
+	(return-from procura-retrocesso-simples (values p testes-total))
+	(let* ((v (first (psr-variaveis-nao-atribuidas p)))
+	       (d (psr-variavel-dominio p v)))
+	  (dolist (valor d)
+	    (multiple-value-bind (consistente testes) (psr-atribuicao-consistente-p p v valor)
+	      (incf testes-total testes)
+	      (when consistente
+		(psr-adiciona-atribuicao! p v valor)
+		(multiple-value-bind (recurs-consistente recurs-testes) (procura-retrocesso-simples p)
+		  (incf testes-total recurs-testes)
+		  (when (not (null recurs-consistente))
+		    (return-from procura-retrocesso-simples (values p testes-total)))))
+	      (psr-altera-dominio! p v d)))
+	  (return-from procura-retrocesso-simples (values nil testes-total))))))
+
 
 ;;; resolve-simples: array -> array
-
+(defun resolve-simples (tab)
+  ""
+  (multiple-value-bind (p) (procura-retrocesso-simples (fill-a-pix->psr tab))
+    (if (null p) nil (psr->fill-a-pix p (array-dimension tab 0) (array-dimension tab 1)))))
 
 
